2277d22acab7202d56fed83f1362efc6b210f190
Adrian <aaadrianrutkaaa@o2.pl>
2023-05-12 17:30:46 +0200

    Updated Jobsearch and actionbutton

--- job-search/src/components/JobSearchForm.vue
+++ job-search/src/components/JobSearchForm.vue
@@ -5,6 +5,7 @@
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute -top-10 left-0">Role</label>
         <input
+          v-model="role"
           type="text"
           placeholder="Software engineer"
           class="w-full text-lg font-normal focus:outline-none"
@@ -19,6 +20,7 @@
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute -top-10 left-0">Where?</label>
         <input
+          v-model="location"
           type="text"
           placeholder="Los Angeles"
           class="w-full text-lg font-normal focus:outline-none"
@@ -37,6 +39,12 @@ export default {
   name: 'JobSearchForm',
   components: {
     ActionButton
+  },
+  data() {
+    return {
+      role: '',
+      location: ''
+    }
   }
 }
 </script>
